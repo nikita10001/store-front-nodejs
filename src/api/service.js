66ae89b6891f0e4e1877a84c332b9f9adeb68756
nikita10001 <n_ryabtsev1@mail.ru>
2023-11-07 19:14:20 +0300

    before deploy

--- src/api/service.js
+++ src/api/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const $host = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://store-backend-nodejs.onrender.com',
   // baseURL: 'http://localhost:3001',
   headers: {
     'Content-Type': 'application/json',
