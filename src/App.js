b626a723043cd45841d6617910414e8473bfc3b9
nikita10001 <n_ryabtsev1@mail.ru>
2023-11-13 09:52:41 +0300

    fix loading

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import { useEffect, useState } from 'react';
 import Preloader from './components/UI/Preloader';
 
 function App() {
-  const { user } = useSelector(selectAuth);
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
+  const { isLoading } = useSelector(selectAuth);
+  console.log(isLoading);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(checkAuth());
-    setIsLoading(false);
   }, []);
   if (isLoading) {
     return <Preloader />;
--- src/store/slices/authSlice.js
+++ src/store/slices/authSlice.js
@@ -41,6 +41,7 @@ const initialState = {
   isAuth: false,
   error: null,
   user: null,
+  isLoading: true,
   editingDeviceId: null,
   modalVisible: false,
 };
@@ -82,13 +83,18 @@ const authSlice = createSlice({
       state.user = null;
       state.error = action.payload;
     });
+    builder.addCase(checkAuth.pending, (state, action) => {
+      state.isLoading = true;
+    });
     builder.addCase(checkAuth.fulfilled, (state, action) => {
       state.user = action.payload;
+      state.isLoading = false;
       state.isAuth = true;
     });
     builder.addCase(checkAuth.rejected, (state, action) => {
       state.user = null;
       state.isAuth = false;
+      state.isLoading = false;
       state.error = action.payload;
     });
   },
