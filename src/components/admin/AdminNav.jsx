ad65b9886d68947e851bb149e15205ce9ac76316
nikita10001 <n_ryabtsev1@mail.ru>
2023-11-19 07:55:54 +0300

    temp save

---
+++ src/components/admin/AdminNav.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ROUTE_PATHS } from '../../router';
+import { NavLink } from 'react-router-dom';
+
+const AdminNav = ({ isAdmin }) => {
+  return (
+    isAdmin && (
+      <div className="header__admin admin-nav">
+        <div className="admin-nav__container">
+          <ul className="admin-nav__menu">
+            <li className="admin-nav__item">
+              <NavLink to={ROUTE_PATHS.ADMIN_COMMENTS} className="admin-nav__link">
+                Комментарии
+              </NavLink>
+            </li>
+            <li className="admin-nav__item">
+              <NavLink to={ROUTE_PATHS.ADMIN} className="admin-nav__link">
+                Товары
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default AdminNav;
